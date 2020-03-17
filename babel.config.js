3f2c81fcf95027831d515802c2e4a09f45adc85d
hieupq <hieu.phq@gmail.com>
2020-03-17 10:50:30 +0700

    Init project

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  env: {
+    production: {},
+  },
+  plugins: [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        legacy: true,
+      },
+    ],
+    ["@babel/plugin-proposal-optional-catch-binding"],
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        alias: {
+          "test/*": ["./test/"],
+        },
+      },
+    ],
+  ],
+}
